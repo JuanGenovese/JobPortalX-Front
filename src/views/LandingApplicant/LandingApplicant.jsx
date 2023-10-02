ec1b9c89d64568fe6d20ae3412b65f3f25d10905
JuanCruz <juancruzgenovese22@gmail.com>
2023-10-02 00:39:05 -0300

    Nuevo diseño de landing applicant

--- src/views/LandingApplicant/LandingApplicant.jsx
+++ src/views/LandingApplicant/LandingApplicant.jsx
@@ -9,83 +9,77 @@ import { getUserDetail } from '../../Redux/Actions/actionsFunction/actionsUsers'
 export default function LandingApplicant({ setValidateState, setCurrentUserStore2 }) {
   const dispatch = useDispatch()
   const userType2 = JSON.parse(localStorage.getItem("currentUser2"));
-  const [greeting, setGreeting] = useState(`Hola, ${userType2.name}!`);
+  const [greeting, setGreeting] = useState(`¡Hola, ${userType2.name}!`);
   const userCv = useSelector(state => state.UserDetail)
   useEffect(()=>{
     dispatch(getUserDetail(userType2.id))
   }, [dispatch,userType2.id])
   console.log(userCv.Cv);
   return (
-    <>
-      <div className={styles.container}>
-        <NavBar
-          setValidateState={setValidateState}
-          setCurrentUserStore2={setCurrentUserStore2}
-        ></NavBar>
-        <div className={styles.containerPrincipal}>
-          <div className={styles.saludo}>
-            <h1 className={styles.titulo}>{greeting}</h1>
-            <div></div>
-            <h3 className={styles.info}>Te damos la bienvenida a JobPortalX</h3>
-            <div className={styles.pregunta}>
-              <h4>¿Qué quieres hacer?</h4>
-            </div>
-          </div>
-          <div className={styles.containerButtons}>
-            <Link to="/postulaciones">
-              <button className={styles.Button} title="Mis postulaciones">
-                <span className={styles.ButtonIcon}></span>
-                <span className={styles.ButtonText}>Mis postulaciones</span>
-              </button>
-            </Link>
-            {
-              userCv.Cv ? <></> : 
-              <Link to="/registro-cv">
-              <button className={styles.Button}>
-                <span className={styles.ButtonIcon}></span>
-                <span className={styles.ButtonText}>Crear CV</span>
-              </button>
-            </Link>
-            }
-            <Link to="/registro-experiencia">
-              <button className={styles.Button}>
-                <span className={styles.ButtonIcon}></span>
-                <span className={styles.ButtonText}>Registrar experiencia</span>
-              </button>
-            </Link>
-            <Link to="/empleos">
-              <button className={styles.Button}>
-                <span className={styles.ButtonIcon}></span>
-                <span className={styles.ButtonText}>Vacantes</span>
-              </button>
-            </Link>
-            <Link to="/Miperfil">
-              <button className={styles.Button}>
-                <span className={styles.ButtonIcon}></span>
-                <span className={styles.ButtonText}>Mi perfil</span>
-              </button>
-            </Link>
-            <Link to="/profiles-company">
-              <button className={styles.Button}>
-                <span className={styles.ButtonIcon}></span>
-                <span className={styles.ButtonText}>Perfiles de empresas</span>
-              </button>
-            </Link>
-            <Link to="/registro-estudio">
-              <button className={styles.Button}>
-                <span className={styles.ButtonIcon}></span>
-                <span className={styles.ButtonText}>Registrar educacion</span>
-              </button>
-            </Link>
-            <Link to="/PlansAndPrices">
-              <button className={styles.Button}>
-                <span className={styles.ButtonIcon}></span>
-                <span className={styles.ButtonText}>Planes y Precios</span>
-              </button>
-            </Link>
-          </div>
+    <div>
+      <NavBar
+        setValidateState={setValidateState}
+        setCurrentUserStore2={setCurrentUserStore2}
+      ></NavBar>
+      <div className={styles.containerPrincipal}>
+        <div className={styles.saludo}>
+          <h1 className={styles.titulo}>{greeting}</h1>
+          <h3 className={styles.info}>Te damos la bienvenida a JobPortalX</h3>
+        </div>
+        <div className={styles.containerButtons}>
+          <Link to="/postulaciones">
+            <button className={styles.Button} title="Mis postulaciones">
+              <span className={styles.ButtonIcon}></span>
+              <span className={styles.ButtonText}>MIS POSTULACIONES</span>
+            </button>
+          </Link>
+          {
+            userCv.Cv ? <></> : 
+            <Link to="/registro-cv">
+            <button className={styles.Button2}>
+              <span className={styles.ButtonIcon}></span>
+              <span className={styles.ButtonText}>CREAR CV</span>
+            </button>
+          </Link>
+          }
+          <Link to="/registro-experiencia">
+            <button className={styles.Button}>
+              <span className={styles.ButtonIcon}></span>
+              <span className={styles.ButtonText}>REGISTRAR EXPERIENCIA</span>
+            </button>
+          </Link>
+          <Link to="/empleos">
+            <button className={styles.Button2}>
+              <span className={styles.ButtonIcon}></span>
+              <span className={styles.ButtonText}>VACANTES</span>
+            </button>
+          </Link>
+          <Link to="/Miperfil">
+            <button className={styles.Button}>
+              <span className={styles.ButtonIcon}></span>
+              <span className={styles.ButtonText}>MI PERFIL</span>
+            </button>
+          </Link>
+          <Link to="/profiles-company">
+            <button className={styles.Button2}>
+              <span className={styles.ButtonIcon}></span>
+              <span className={styles.ButtonText}>PERFILES EMPRESAS</span>
+            </button>
+          </Link>
+          <Link to="/registro-estudio">
+            <button className={styles.Button}>
+              <span className={styles.ButtonIcon}></span>
+              <span className={styles.ButtonText}>REGISTRAR EDUCACION</span>
+            </button>
+          </Link>
+          <Link to="/PlansAndPrices">
+            <button className={styles.Button2}>
+              <span className={styles.ButtonIcon}></span>
+              <span className={styles.ButtonText}>PLANES Y PRECIOS</span>
+            </button>
+          </Link>
         </div>
       </div>
-    </>
+    </div>
   );
 }
